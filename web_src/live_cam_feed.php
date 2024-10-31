d9309db73f6360963084d740d047a7b586f59ea4
LeifHoffman <hoffmanl2@etown.edu>
2024-10-31 11:01:59 -0400

    Updated live_cam_feed to run camera py properly

--- web_src/live_cam_feed.php
+++ web_src/live_cam_feed.php
@@ -2,13 +2,16 @@
 session_start();
 ?>
 
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>●Live Robot Camera</title>
 </head>
-<?PHP
-echo shell_exec("python test.py 'parameter1'");
-?>
+<body>
+    <h1>This page will display the live camera feed from the robot.</h1>
+</body>
 </head>
+<?PHP
+echo shell_exec(escapeshellcmd('camera.py'));
+?>
