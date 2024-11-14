c5ab6c99545f1c241ef30e7e002782abe81276a1
LeifHoffman <hoffmanl2@etown.edu>
2024-11-14 09:09:59 -0500

    Added qr code generator and reader

---
+++ data_src/qrcode_generator.py
@@ -0,0 +1,9 @@
+# QR Code Generator
+
+import segno
+
+#Generate QR Codes
+for i in range(4):
+    qrcode = segno.make_qr(str(i+1))
+    qrcode.save("robot_qrcode"+str(i+1)+".png",
+                scale=5)
