a0db945e8deeb0790c59ee51cb28c85665bac7a1
Owen Yang <owy.yang@gmail.com>
2024-11-17 19:22:04 -0500

    Starting login process

--- data_src/users/read.php
+++ data_src/users/read.php
@@ -1,3 +1,22 @@
 <?php
 //This page will be used to read user information to allow registered users to log in to the system and make orders
+include "../includes/db_config.php";
+session_start();
+
+$user = $_SESSION("username");
+
+$connection = new mysqli($servername, $username, $password, $database);
+
+if($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+$sql = "SELECT * FROM users WHERE username = ?";
+$sql->bind_param("?", $user);
+$sql.execute();
+$result = $sql->get_result();
+
+if($result->num_rows > 0){
+
+}
 ?>
--- web_src/login.php
+++ web_src/login.php
@@ -3,24 +3,11 @@ session_start();
 
 $password=$_POST["password"];
 $username=$_POST["username"];
+$_SESSION["username"] = $username;
 
-$host = $_SERVER['HTTP_HOST'];
-$protocol = $_SERVER['PROTOCOL'] = isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https' : 'http';
-$file_path = substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['REQUEST_URI'], '/') + 1);
-$url = $protocol."://".$host.$file_path;
-$web_string = file_get_contents($url."data/user/read.php?username=".$username);
-$users = json_decode($web_string);
-if (is_array($users) && count($users) > 0) {
+//Send username to read.php in users and check if user existst and password match
 
-    $user = array_pop($users);
-    if($user->password==$pass){
-        //you are logged in
-        $_SESSION["LoggedIn"] = "YES";
-        $_SESSION["username"] = $username;
-        header("location:index.html");
-        exit;
-    }
-}
+
 
 $_SESSION["LoggedIn"] = "NO";
 $_SESSION["username"] = "";
